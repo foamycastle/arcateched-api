9f762ff09dfde6d55b88e2ca1fb523102584de83
Aaron <unclepong@gmail.com>
2024-02-14 17:37:13 -0600

    added byMultipleTypes module
    queries for contacts that have multiple types of contact types

---
+++ src/api/get/contacts/byMultipleTypes/byMultipleTypes.ts
@@ -0,0 +1,16 @@
+import {prismaClient} from "../../../../prisma/prismaClient";
+import Prisma from '@prisma/client'
+
+export default async function (types:Array<Prisma.contactType>){
+    return prismaClient.contacts.findMany({
+        where:{
+            type:{
+                hasEvery:types
+            }
+        },
+        select:{
+            id:true,
+            name:true
+        }
+    })
+}
