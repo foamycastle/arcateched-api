55434c804abd935c9b501c9794807389a67428e8
Aaron <unclepong@gmail.com>
2024-02-15 21:56:50 -0600

    changed route paths
    altered the anatomy of some of the route paths

--- src/rsc/getRouterPaths.ts
+++ src/rsc/getRouterPaths.ts
@@ -1,8 +1,8 @@
 
 export const manyMachinesByIdPath: string = '/machine_data/manyMachinesById';
 export const machineNamesPath: string = '/machine_data/names';
-export const machineByIdPath: string = '/machine_data/machine/:id';
-export const machineByOpStatePath: string = '/machine_data/byOpState/:opState';
+export const machineByIdPath: string = '/machine_data/:id';
+export const machineByOpStatePath: string = '/machine_data/opState/:opState';
 export const contactsByTypePath: string = '/contacts/byType/:contactType';
 export const contactsByManyTypesPath:string = '/contacts/byManyTypes';
 
