0939eb854bc0e34d50a637fcde0a93c7bb311735
Aaron <unclepong@gmail.com>
2024-02-15 23:51:26 -0600

    return object
    added the game type to the return results

--- src/CRUD/machine_data/read_byGameType.ts
+++ src/CRUD/machine_data/read_byGameType.ts
@@ -1,16 +1,18 @@
 import Prisma from "@prisma/client";
 import {prismaClient} from "../../prisma/prismaClient";
 
-export default async function (type:Prisma.$Enums.gameType[]){
+export default async function (gameType: Prisma.$Enums.gameType[]){
+
     return prismaClient.machine_data.findMany({
-        where:{
-            gameType: {
-                hasEvery:type
+        where: {
+            gameType:{
+                hasEvery:gameType
             }
         },
         select:{
             id:true,
-            name:true
+            name:true,
+            gameType:true
         }
     })
 }
