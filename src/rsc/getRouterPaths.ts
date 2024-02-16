1ce661ab77389cbdea2e555dd5c509b9ac19f1ee
Aaron <unclepong@gmail.com>
2024-02-15 21:43:59 -0600

    changed route paths
    some of the GET route paths were accidentally altered during a recent search and replace

--- src/rsc/getRouterPaths.ts
+++ src/rsc/getRouterPaths.ts
@@ -1,7 +1,7 @@
 
 export const manyMachinesByIdPath: string = '/machine_data/manyMachinesById';
-export const machineNamesPath: string = '/machine_data/read_names';
-export const machineByIdPath: string = '/machine_data/read_machine/:id';
+export const machineNamesPath: string = '/machine_data/names';
+export const machineByIdPath: string = '/machine_data/machine/:id';
 export const machineByOpStatePath: string = '/machine_data/byOpState/:opState';
 export const contactsByTypePath: string = '/contacts/byType/:contactType';
 export const contactsByManyTypesPath:string = '/contacts/byManyTypes';
