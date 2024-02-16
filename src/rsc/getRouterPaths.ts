a6d6a3331de5380dc249ca9d91d60687a2d31c83
Aaron <unclepong@gmail.com>
2024-02-15 23:50:38 -0600

    path confusion
    fixed an issue where express was mistaking path segments in the URL from parameters that it need to validate and therefore causing some routes to hang

--- src/rsc/getRouterPaths.ts
+++ src/rsc/getRouterPaths.ts
@@ -1,9 +1,9 @@
 
 export const manyMachinesByIdPath: string = '/machine_data/manyMachinesById';
 export const machineNamesPath: string = '/machine_data/names';
-export const machineByIdPath: string = '/machine_data/:id';
+export const machineByIdPath: string = '/machine_data/id/:id';
 export const machineByOpStatePath: string = '/machine_data/opState/:opState';
-export const machineByGameType: string = '/machine_data/gameType/:gameType';
+export const machineByGameType: string = '/machine_data/gameType';
 export const contactsByTypePath: string = '/contacts/byType/:contactType';
 export const contactsByManyTypesPath:string = '/contacts/byManyTypes';
 
