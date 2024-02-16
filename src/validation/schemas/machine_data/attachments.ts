41b216ed178e6e3c9f9f1ff6b5d87bb69a6c2ea6
Aaron <unclepong@gmail.com>
2024-02-15 21:14:49 -0600

    object shape
    converted object shape into a weak generic

--- src/validation/schemas/machine_data/attachments.ts
+++ src/validation/schemas/machine_data/attachments.ts
@@ -1,13 +1,11 @@
 import Joi from "joi";
-import Prisma from "@prisma/client";
+import {Prisma} from "@prisma/client";
 import {UUID} from "../UUID";
 
-export const attachments =
-    Joi.object<Prisma.Prisma.attachmentsCreateNestedManyWithoutMachine_dataInput>({
+export const attachments = Joi.object<Prisma.attachmentsCreateNestedManyWithoutMachine_dataInput>({
+    create:Joi.object().optional(),
+    createMany:Joi.object().optional(),
+    connectOrCreate:Joi.object().optional(),
+    connect:Joi.object().optional()
+})
 
-        createMany:Joi.object().optional(),
-        connect:Joi.object().optional(),
-        create:Joi.object().optional(),
-        connectOrCreate:Joi.object().optional()
-
-    })
