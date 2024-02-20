c6502639b7276fb09c28f9cbb3a25a9fa028d9c8
Aaron <unclepong@gmail.com>
2024-02-20 07:06:40 -0600

    added check headerSent test

--- src/root_error_handler.ts
+++ src/root_error_handler.ts
@@ -1,7 +1,11 @@
 import {ArcatechedAPIRuntimeError} from "./errorObjects/ArcatechedAPIRuntimeError";
+import {NextFunction} from "express";
 
-export default function (err:any,req:any,res:any){
+export default function (err:any,req:any,res:any,next:NextFunction){
 
+    if(res.headersSent){
+        next(err)
+    }
     if(err instanceof ArcatechedAPIRuntimeError){
         res.status(err.code).json(err.responseObject())
     }
