fb9d1feecc419aebd135a99c5be93b4723d6a3af
Aaron <unclepong@gmail.com>
2024-02-28 06:54:37 -0600

    remove getUIFields
    removed the endpoint from the Machine Data model

--- src/PrismaModel/MachineData/getUIFields/preparedQuery.ts
+++
@@ -1,6 +0,0 @@
-import {Prisma} from "@prisma/client";
-
-const preparedQuery = () => {
-    return Prisma.validator<>()({})
-}
-export default preparedQuery
--- src/PrismaModel/MachineData/getUIFields/validation.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-import Prisma from "@prisma/client";
-import * as MachineDataValidation from "../../../validation/model/MachineData"
-
-const inputValidation = Joi.object()
-
-export default inputValidation
